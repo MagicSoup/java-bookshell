6b69e77b722498e6b5e0c34c0c48529e6b25dbed
MagicSoup <championlouis@hotmail.com>
2024-06-22 16:40:32 +0200

    Init project

---
+++ src/test/java/com/magicsoup/spring/data/SpringData/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicsoup.spring.data.SpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
